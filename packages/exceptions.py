66a9a941476a8df74abfeecd3448291684c507d4
Brian Alexander <balexander04@gmail.com>
2015-08-30 16:44:31 -0500

    Adding a packages dir and included an exceptions class.

---
+++ packages/exceptions.py
@@ -0,0 +1,9 @@
+'''These are the exception classes.'''
+
+
+class NoOptionFoundException(Exception):
+    '''
+       The user attempted to select an
+       option that is not available.
+    '''
+    pass
